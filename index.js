6d54cb6a7ee2b447207729f6299b6d99867b73ee
Naveen Kumar <kumarn.na20@gmail.com>
2023-09-27 01:34:46 +0530

    added models and setup db connection

--- index.js
+++ index.js
@@ -2,6 +2,18 @@ require('dotenv').config();
 
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+
+//DB Connection
+mongoose
+  .connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+  })
+  .then(() => {
+    console.log("DB CONNECTED");
+  });
 
 // adding default port
 const port = process.env.PORT || 8000;

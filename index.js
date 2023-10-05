c86103a3951b62cfd915591dd27a972318c038e2
Naveen Kumar <kumarn.na20@gmail.com>
2023-10-06 01:11:20 +0530

    added routes with middlewares

--- index.js
+++ index.js
@@ -3,6 +3,18 @@ require('dotenv').config();
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const compression = require('compression');
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+
+// Routes
+const authRoutes = require("./routes/auth");
+const userRoutes = require("./routes/user");
+const categoryRoutes = require("./routes/category");
+const productRoutes = require("./routes/product");
+const orderRoutes = require("./routes/order");
+const stripeRoutes = require("./routes/stripePayment")
 
 //DB Connection
 mongoose
@@ -14,6 +26,20 @@ mongoose
   .then(() => {
     console.log("DB CONNECTED");
   });
+
+//Middlewares
+app.use(compression())
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(cors());
+
+//My Routes
+app.use("/api", authRoutes);
+app.use("/api", userRoutes);
+app.use("/api", categoryRoutes);
+app.use("/api", productRoutes);
+app.use("/api", orderRoutes);
+app.use("/api", stripeRoutes);
 
 // adding default port
 const port = process.env.PORT || 8000;
